6debffbacddc01ff58cbcd73128e94155bf88d7b
raihan <raihan.zbr@gmail.com>
2025-04-12 11:24:26 +0200

    Search result view added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -33,7 +33,7 @@ const Rootlayout = () => {
         <Stack.Screen name='index' options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }}/> */}
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }}/>
       </Stack>
     </ GlobalProvider>
   )
