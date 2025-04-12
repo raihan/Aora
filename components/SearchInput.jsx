20fec6ebc2d80eb30a7fc200360d73f021577d65
raihan <raihan.zbr@gmail.com>
2025-04-12 11:23:43 +0200

    Search field updated

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
-import { View, Text, TextInput, Touchable, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TextInput, Touchable, TouchableOpacity, Image, Alert } from 'react-native';
 import React from 'react'
 import { icons } from '../constants';
+import { router, usePathname } from 'expo-router';
 
-const SearchInput = ({
-  title, 
-  value, 
-  placeholder, 
-  handleChangeText,  
-  otherStyles, 
-  ...props
-}) => {
-  const [showPassword, setshowPassword] = useState(false)
+const SearchInput = ({ initialQuery }) => {
+  const pathname = usePathname()
+  const [query, setQuery] = useState(initialQuery || '')
   
   return (
       <View className="border-2 border-black-200 w-full 
@@ -19,16 +14,26 @@ const SearchInput = ({
       focus:border-secondary items-center flex-row space-x-4">
         <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
-        value={value} 
+        value={query} 
         placeholder="Search for a video topic"
-        placeholderTextColor="#7b7b8b"
-        onChangeText={handleChangeText}
-        secureTextEntry={title === 'Password' && 
-          (!showPassword)}
-        {...props}
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
         />
 
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            if(!query) {
+              return Alert.alert("Missing query", 
+                "Please input something to search results acros database")
+            }
+
+            if (pathname.startsWith('/search')) {
+              router.setParams({ query })
+            } else {
+              router.push(`/search/${query}`)
+            }
+          }}
+        >
             <Image
             source={icons.search}
             className='w-5 h-5'
