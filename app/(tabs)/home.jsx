b68415d5969fbf16913c2249e756769835a81fa4
raihan <raihan.zbr@gmail.com>
2025-04-16 17:13:33 +0200

    user info added on home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,8 +9,10 @@ import EmptyState from '../../components/EmptyState';
 import VideoCard from '../../components/VideoCard';
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext()
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
@@ -35,8 +37,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back</Text>
-                <Text className="text-2xl font-psemibold text-white">JSMastery</Text>
+                  Welcome back,</Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  {user?.username}
+                  </Text>
               </View>
 
               <View className="mt-1.5 ">
